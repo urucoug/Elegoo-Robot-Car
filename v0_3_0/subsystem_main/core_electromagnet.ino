db37f59f3f19af0e5c5d34f7cb2ec4bf0fb9250b
Gabriel Staples <ercaguy@gmail.com>
2017-05-29 23:45:27 -0400

    MAJOR MAJOR revamp of code into v0_3_0
    See "Elegoo-Robot-Car - History.txt" for details

---
+++ v0_3_0/subsystem_main/core_electromagnet.ino
@@ -0,0 +1,11 @@
+void _electromagnetOn() 
+{
+  //turn the elecromagnet on
+  //GS note to Kevin: here's the answer: digitalWrite(electroMagnetPin, HIGH); //DONE! THAT'S IT!
+}
+
+void _electromagnetOff() 
+{
+  //turn the elecromagnet off
+  //GS note to Kevin: here's the answer: digitalWrite(electroMagnetPin, LOW); //DONE! THAT'S IT!
+}
--- v3/Subsystem_main/Electromagnet_core_functions.ino
+++
@@ -1,7 +0,0 @@
-void _electromagnetOn() {
-  // turn the elecromagnet on
-}
-
-void _electromagnetOff() {
-  // turn the elecromagnet off
-}
